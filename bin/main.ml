d91bd11b4309b02edc9a7032b1e8c75a6878bae2
Vladimir Kidyaev <krios2146@gmail.com>
2024-08-10 02:09:25 +0700

    feat(bin): update main.ml to create and render simulation

--- bin/main.ml
+++ bin/main.ml
@@ -1,1 +1,6 @@
-let () = print_endline "Hello, World!"
+let simulation =
+  Simulation.create_simluation ~map_height:10 ~map_width:10
+    ~density:Simulation.Low
+in
+
+Simulation.render_from_start simulation
