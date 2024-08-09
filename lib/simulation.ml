6961132f49f2710833f837d5b837d13d219e8745
Vladimir Kidyaev <krios2146@gmail.com>
2024-08-10 02:09:01 +0700

    feat(lib): add random entity generator & fix rendering

--- lib/simulation.ml
+++ lib/simulation.ml
@@ -1,6 +1,7 @@
 type creature = Herbivore | Predator
 type entity = Tree | Grass | Rock | Creature of creature
 type coordinates = { x : int; y : int }
+type density = High | Medium | Low
 
 module CoordinatesMap = Map.Make (struct
   type t = coordinates
@@ -32,9 +33,7 @@ let rec render simulation coordinates (to_render : string) =
   | _ -> (
       match (coordinates.x, simulation.max_x) with
       | x, max_x when x >= max_x ->
-          render simulation
-            { coordinates with y = coordinates.y + 1 }
-            (to_render ^ "\n")
+          render simulation { x = 0; y = coordinates.y + 1 } (to_render ^ "\n")
       | _ ->
           let entity = find_entity_by_coordinates simulation.map coordinates in
           let sprite =
@@ -47,3 +46,61 @@ let rec render simulation coordinates (to_render : string) =
       )
 
 let render_from_start simulation = render simulation { x = 0; y = 0 } ""
+
+let gen_random_coordinates (max_x : int) (max_y : int) =
+  let x = Random.int max_x in
+  let y = Random.int max_y in
+  { x; y }
+
+let is_occupied coordinates coordinates_entity_map =
+  CoordinatesMap.mem coordinates coordinates_entity_map
+
+let generate amout entity = List.init amout (fun _ -> entity)
+
+let rec place coordinates_entity_map (entities : entity list) max_x max_y =
+  match entities with
+  | [] -> coordinates_entity_map
+  | head :: tail ->
+      let coordinates = gen_random_coordinates max_x max_y in
+      if is_occupied coordinates coordinates_entity_map then
+        place coordinates_entity_map entities max_x max_y
+      else
+        let updated_map =
+          CoordinatesMap.add coordinates head coordinates_entity_map
+        in
+        place updated_map tail max_x max_y
+
+let percent value area = area * value / 100
+
+let create_simluation ~(map_height : int) ~(map_width : int)
+    ~(density : density) =
+  let place coordinates_entity_map entities =
+    place coordinates_entity_map entities map_width map_height
+  in
+
+  let map : coordinates_entity_map = CoordinatesMap.empty in
+  let area = map_width * map_height in
+  let percent value = percent value area in
+
+  let map =
+    match density with
+    | High ->
+        let map = place map (generate (percent 40) Grass) in
+        let map = place map (generate (percent 10) Tree) in
+        let map = place map (generate (percent 5) Rock) in
+        let map = place map (generate (percent 3) (Creature Predator)) in
+        place map (generate (percent 5) (Creature Herbivore))
+    | Medium ->
+        let map = place map (generate (percent 20) Grass) in
+        let map = place map (generate (percent 5) Tree) in
+        let map = place map (generate (percent 2) Rock) in
+        let map = place map (generate (percent 2) (Creature Predator)) in
+        place map (generate (percent 4) (Creature Herbivore))
+    | Low ->
+        let map = place map (generate (percent 10) Grass) in
+        let map = place map (generate (percent 4) Tree) in
+        let map = place map (generate (percent 2) Rock) in
+        let map = place map (generate (percent 1) (Creature Predator)) in
+        place map (generate (percent 2) (Creature Herbivore))
+  in
+  { map; max_x = map_width; max_y = map_height }
