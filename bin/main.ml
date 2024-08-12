95287864969c8d9c33323ab3d2d9ca647654ceba
Vladimir Kidyaev <krios2146@gmail.com>
2024-08-12 23:32:09 +0700

    feat(bin): start simulation instead of rendering it in the main.ml

--- bin/main.ml
+++ bin/main.ml
@@ -1,6 +1,6 @@
 let simulation =
-  Simulation.create_simluation ~map_height:10 ~map_width:10
+  Simulation.create_simluation_map ~map_height:10 ~map_width:10
     ~density:Simulation.Low
 in
 
-Simulation.render_from_start simulation
+Simulation.start simulation
